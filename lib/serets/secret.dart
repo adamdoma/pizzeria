7021c86fece8e838387fa529aadb493f20e3929b
Adam <adamdoma@gmail.com>
2021-11-13 18:42:03 +0200

    Delete secret.dart

--- lib/serets/secret.dart
+++
@@ -1,7 +0,0 @@
-class Secret {
-  //
-  String paypalClientId =
-      'AVW2S0iRLFM9tFMGY1uCo6XEBYpzPdDV5kZdOp2pSmLsQE9ThqwCRyh7G76c90yKjC0IbIToLt4DqyOE';
-  String paypalSecret =
-      'EAOTC8R8dSGUc3vIv7GAlOxTO1bIXVUhFJJHnJbPXBS277QnHaku_DgiwD4QxYSOGNZUn4uXrJHZclcV';
-}
